1456bfde48c5fccc648ae28ee0a11e0af4ba26d2
Viktor Romats <vctrrmts@gmail.com>
2024-03-16 16:48:18 +0300

    Auth

--- Todos.Service/Dto/UpdateToDoDto.cs
+++ Todos.Service/Dto/UpdateToDoDto.cs
@@ -9,7 +9,6 @@ namespace Todos.Service.Dto
     public class UpdateToDoDto
     {
         public string Label { get; set; } = default!;
-        public int OwnerId { get; set; }
         public bool IsDone { get; set; }
 
     }
